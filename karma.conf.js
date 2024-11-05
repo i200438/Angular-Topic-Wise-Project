6b3e3c006b9e007511b9b7ef6ae7944249f872ac
i200438 <ch.talha.tanveer83@gmail.com>
2024-11-05 21:59:15 +0500

    Create karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,18 @@
+// karma.conf.js
+module.exports = function (config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadless'],
+    singleRun: true
+  });
+};
